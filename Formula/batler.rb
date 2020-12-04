0bc5d7bb0050f7305ef131a413f30396e65d8275
Vitor Barbosa <vitor.araujo@wildlifestudios.com>
2020-12-04 19:27:23 -0300

    Add batler@0.3.0

--- Formula/batler.rb
+++ Formula/batler.rb
@@ -1,14 +1,14 @@
 class Batler < Formula
   desc "CLI to run tests and display coverage for iOS applications"
   homepage "https://github.com/vitorbaraujo/batler"
-  url "https://github.com/vitorbaraujo/batler/archive/0.2.0.tar.gz"
-  sha256 "c966f65ff0f7236dc1e614d3d61bdef8548e8035d0e5b8f73c0d327c4bec3757"
+  url "https://github.com/vitorbaraujo/batler/archive/0.3.0.tar.gz"
+  sha256 "076fb75d2f783327ae6d76b5889060d682792158f34a74076428a8f6d5a8ba83"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", "build/batler", "./cmd/batler/"
+    system "make", "build"
     bin.install "build/batler"
   end
 end
